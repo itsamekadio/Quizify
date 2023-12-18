0483bb6c9f5d85785ad5a8f7be4ac5e6acef2083
itsamekadio <135104188+itsamekadio@users.noreply.github.com>
2023-12-18 11:51:46 +0200

    Add files via upload

---
+++ quizify/Pages/AdminPermissions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quizzify.Pages
+{
+    public class AdminPermissionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ quizify/Pages/AdminPosts.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Quizzify.Pages
+{
+    public class AdminPostsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost(int id)
+        {
+            if (id == 1)
+            {
+                //implement the function of  making post or announcement
+            }
+            else if (id == 2)
+            {
+                //impelment the function to edit the post
+            }
+            else if (id == 3)
+            {
+                //impelment the function to remove post
+            }
+            else if (id == 4)
+            {
+                //impelment the function to make a comment
+            }
+            return Page();
+        }
+    }
+}
