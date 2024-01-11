0efaf4cd134547468794464a8e7f8de6a8629045
itsamekadio <135104188+itsamekadio@users.noreply.github.com>
2024-01-11 11:33:31 +0200

    Update SignIn.cshtml.cs
    added tests for admin

--- quizify/Pages/SignIn.cshtml.cs
+++ quizify/Pages/SignIn.cshtml.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Quizzify.Pages.classes;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
 
 namespace Quizzify.Pages
 {
@@ -18,12 +20,53 @@ namespace Quizzify.Pages
 
         static string ConString = @"Data Source=ABDELRAHMAN-ELK;Initial Catalog=yarab1;Integrated Security=True";
         SqlConnection con = new SqlConnection(ConString);
-        
+        public string whattotest { get; set; }
+        public bool test(string email,string pass)
+        {
+            con.Open();
+            string querystring = "Select  count(*) from AdminData where Email='" + email + "' and AdminPassword='" + pass + "'";
+            SqlCommand cmd1 = new SqlCommand(querystring, con);
+            string querystring2 = "Select  count(*) from PlayerData where Email='" + email + "' and playerPassword='" + pass + "'";
+            SqlCommand cmd2 = new SqlCommand(querystring2, con);
+            int countplayer = (int)cmd2.ExecuteScalar();
+            int countadmin = (int)cmd1.ExecuteScalar();
+            if (countadmin > 0)
+            {
+                con.Close();
+                return true;
+            }
+
+            if (countplayer > 0)
+            {
+                con.Close();
+                return true;
+            }
+            else
+            {
+                con.Close(); return false;
+            }
+        }
         public void OnGet()
-        {        }
+        { Console.WriteLine("testing sign up before trying it to avoid errors");
+            if(test("elkad@vbnhjmk.com", "456123"))
+            {
+                Console.WriteLine("passed");
+            }
+            else Console.WriteLine("failed");
+
+            if (test("ss", "adfaf"))
+            {
+                Console.WriteLine("passed");
+            }
+            else Console.WriteLine("failed");
+
+           
+        }
         public IActionResult OnPost()
 
         {
+        
+
             try
             { con.Open();
                 string querystring = "Select  count(*) from AdminData where Email='" +email + "' and AdminPassword='"+pass+"'";
@@ -76,11 +119,8 @@ namespace Quizzify.Pages
 
 
                 }
-               /* else if(countauthor>0)
-                {
-                    return RedirectToPage("/SignIn");
-
-                }*/
+                
+               
 
 
 
@@ -101,4 +141,5 @@ namespace Quizzify.Pages
 
         }
     }
+    
 }
